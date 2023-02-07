90a4a4008fd86398c9cfd61cac16fe4ffd79139d
Pascoal Bayonne <eddybayonne@gmail.com>
2023-02-07 20:32:25 +0000

    started microservice decision

---
+++ src/main/java/pt/bayonne/sensei/decision/DecisionApplication.java
@@ -0,0 +1,13 @@
+package pt.bayonne.sensei.decision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/bayonne/sensei/decision/DecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.bayonne.sensei.decision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
